6b28ad35677155bd5f8dc1cffd9a4a5d2fd41ac5
Abhi <abhinav118@gmail.com>
2025-08-19 16:21:53 -0400

    Add components/ActionButton.tsx

---
+++ components/ActionButton.tsx
@@ -0,0 +1,12 @@
+interface ActionButtonProps {
+  size?: any
+  variant?: any
+}
+
+export default function ActionButton({ size, variant }: ActionButtonProps) {
+  return (
+    <div className="">
+      {/* ActionButton component content */}
+    </div>
+  )
+}
