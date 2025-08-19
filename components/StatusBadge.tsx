f60fee23fbe3bd664a9c667f0ee656f373a7e7d7
Abhi <abhinav118@gmail.com>
2025-08-19 16:21:52 -0400

    Add components/StatusBadge.tsx

---
+++ components/StatusBadge.tsx
@@ -0,0 +1,12 @@
+interface StatusBadgeProps {
+  size?: any
+  variant?: any
+}
+
+export default function StatusBadge({ size, variant }: StatusBadgeProps) {
+  return (
+    <div className="">
+      {/* StatusBadge component content */}
+    </div>
+  )
+}
