562a72373f14e914e8a3fec5e622040190680d50
Abhi <abhinav118@gmail.com>
2025-08-19 16:21:52 -0400

    Add components/ProgressCard.tsx

---
+++ components/ProgressCard.tsx
@@ -0,0 +1,12 @@
+interface ProgressCardProps {
+  showProgress?: any
+  actionButton?: any
+}
+
+export default function ProgressCard({ showProgress, actionButton }: ProgressCardProps) {
+  return (
+    <div className="">
+      {/* ProgressCard component content */}
+    </div>
+  )
+}
